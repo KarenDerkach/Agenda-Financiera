920a2a536cb838375c4fab27299be07db9f9823c
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-05-08 08:45:43 -0300

    fix calculator function

--- frontend/src/components/Calculator/Calculator.tsx
+++ frontend/src/components/Calculator/Calculator.tsx
@@ -2,95 +2,64 @@ import React, {useState}from 'react'
 import './Calculator.css'
 
 function Calculator() {
-    const [result, setResult] = useState(0)
-    const [operation, setOperation] = useState('')
-    const [firstNumber, setFirstNumber] = useState('')
-    const [secondNumber, setSecondNumber] = useState('')
-    const handleClickNumber = (num:string) => {
-        if(operation === ''){
-        setFirstNumber(firstNumber + num)
-    }else{
-        setSecondNumber(secondNumber + num)
-    }
-}
-    const handleClickOperation = (op:string) => {
-        setOperation(op)
-    
-    }
-    const handleClickResult = () => {
-  
-        switch(operation){
-            case '+':
-                setResult(parseInt(firstNumber) + parseInt(secondNumber))
-                break
-            case '-':
-                setResult(parseInt(firstNumber) - parseInt(secondNumber))
-                break
-            case '*':
-                setResult(parseInt(firstNumber) * parseInt(secondNumber))
-                break
-            case '/':
-                setResult(parseInt(firstNumber) / parseInt(secondNumber))
-                break
-            case '%':
-                setResult(parseInt(firstNumber) % parseInt(secondNumber))
-                break
-            case '^':
-                setResult(Math.pow(parseInt(firstNumber), parseInt(secondNumber)))
-                break
-            case '√':
-                setResult(Math.sqrt(parseInt(firstNumber)))
-                break
-            default:
-                setResult(0)
-        }
-   
-    }
 
-    const handleClickClear = () => {
-        setResult(0)
-        setOperation('')
-        setFirstNumber('')
-        setSecondNumber('')
-    }
+const [resultado, setResultado] = useState("");
 
-    const handleClickDelete = () => {
-        if(operation === ''){
-            setFirstNumber(firstNumber.substring(0, firstNumber.length - 1))
-        }else{
-            setSecondNumber(secondNumber.slice(0, -1))
-        }
-    }
+const [secondOperation , setSecondOperation] = useState("");
+
+const handleClickDelete = () => {
+  setResultado("");
+};
+
+const handleClickClear= () => {
+  setResultado(resultado.slice(0, -1));
+};
+
+const onChange = (e:any) => {
+  setResultado(resultado.concat(e.target.name));
+};
+
+const handleClickResult= () => {
+  try {
+    // eslint-disable-next-line no-eval
+    setResultado(eval(resultado).toString());
+    setSecondOperation(resultado);
+  } catch (error) {
+    setResultado("Error");
+  }
+};
 
   return (
     <div className='app-header'>
-          {/* <h3 className='app-title'>Calculator</h3> */}
+       
         <div className='app-container'>
           
             <div className='app-input'>
-                <div className='previous-operation'>{operation ? firstNumber + operation + secondNumber : ''} </div>
-                    <div className='current-operation'>{result ? result : (!operation ? firstNumber : secondNumber)}</div>
+                    <div className='previous-operation'>{resultado? secondOperation : null}</div>
+                    <div className='current-operation'>{resultado }</div>
+             
+            
             </div>
             <button className='span-two' onClick={handleClickClear}> AC</button>
             <button className ='span-two'onClick={handleClickDelete}>DEL</button>
-            <button className='btn-op'onClick={() => handleClickOperation('%')}>%</button>
-            <button  className='btn-op'onClick={()=>{handleClickOperation('/')}}>/</button>
-            <button  className='btn-op'onClick={()=>{handleClickOperation('^')}}>^</button>
-            <button  className='btn-op'onClick={()=>{handleClickOperation('√')}}>/</button>
-            <button className='btn-num'onClick={()=>handleClickNumber('7')}>7</button>
-            <button className='btn-num'onClick={()=>handleClickNumber('8')}>8</button>
-            <button className='btn-num'onClick={()=>handleClickNumber('9')}>9</button>
-            <button  className='btn-op'onClick={()=>{handleClickOperation('*')}}>*</button>
-            <button className='btn-num'onClick={()=>handleClickNumber('4')}>4</button>
-            <button className='btn-num'onClick={()=>handleClickNumber('5')}>5</button>
-            <button className='btn-num'onClick={()=>handleClickNumber('6')}>6</button>
-            <button  className='btn-op'onClick={()=>{handleClickOperation('+')}}>+</button>
-            <button className='btn-num'onClick={()=>handleClickNumber('1')}>1</button>
-            <button className='btn-num'onClick={()=>handleClickNumber('2')}>2</button>
-            <button className='btn-num'onClick={()=>handleClickNumber('3')}>3</button>
-            <button  className='btn-op'onClick={()=>{handleClickOperation('-')}}>-</button>
-            <button className='btn-num'onClick={()=>{handleClickNumber('.')}}>.</button>
-            <button className='btn-num'onClick={()=>handleClickNumber('0')}>0</button>
+            <button className='btn-op' name='%'onClick={onChange}>%</button>
+            <button  className='btn-op'name='/'onClick={onChange}>/</button>
+            <button  className='btn-op'name='^'onClick={onChange}>^</button>
+            <button  className='btn-op'name='√'onClick={onChange}>√</button>
+            <button className='btn-num'name='7'onClick={onChange}>7</button>
+            <button className='btn-num'name='8'onClick={onChange}>8</button>
+            <button className='btn-num'name='9'onClick={onChange}>9</button>
+            <button  className='btn-op'name='*'onClick={onChange}>*</button>
+            <button className='btn-num'name='4'onClick={onChange}>4</button>
+            <button className='btn-num'name='5'onClick={onChange}>5</button>
+            <button className='btn-num'name='6'onClick={onChange}>6</button>
+            <button  className='btn-op'name='+'onClick={onChange}>+</button>
+            <button className='btn-num'name='1'onClick={onChange}>1</button>
+            <button className='btn-num'name='2'onClick={onChange}>2</button>
+            <button className='btn-num'name='3'onClick={onChange}>3</button>
+            <button  className='btn-op'name='-'onClick={onChange}>-</button>
+            <button className='btn-num'name='.'onClick={onChange}>.</button>
+            <button className='btn-num'name='0'onClick={onChange}>0</button>
             <button className='span-two' onClick={handleClickResult}>=</button>
         </div>
         
