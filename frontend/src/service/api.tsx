e8ef69b8f38a6a340f722d9fe23e8e6b803c1e1b
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-05-16 20:35:11 -0300

    fix service api frontend

--- frontend/src/service/api.tsx
+++ frontend/src/service/api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL:   'http://localhost:3001' || 'https://agenda-financiera.vercel.app',
+  baseURL:   'http://localhost:3001' || 'https://agenda-aplication.herokuapp.com',
 })
 
 
