5d52348b6a444a937aafc1c7873e848eba21766f
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-05-16 21:07:27 -0300

    heroku config

--- frontend/src/service/api.tsx
+++ frontend/src/service/api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL:   'http://localhost:3001' || 'https://agenda-aplication.herokuapp.com',
+  baseURL:   'http://localhost:3001' || 'https://agile-stream-37029.herokuapp.com',
 })
 
 
