20f6dc0e5ee61793b820d6f794a58ebcf0bb3813
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-05-18 18:18:58 -0300

    config deploy

--- frontend/src/service/api.tsx
+++ frontend/src/service/api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL:   'http://localhost:3001' || process.env.REACT_APP_URL_HEROKU,
+  baseURL:   process.env.REACT_APP_URL_HEROKU||'http://localhost:3001' ,
 })
 
 
